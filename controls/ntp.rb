bf5d7ec302b7186e823692d4086b3bf5071fcd84
juju4 <juju4@users.noreply.github.com>
2018-10-07 09:26:44 -0400

    more rubocop cleaning

--- controls/ntp.rb
+++ controls/ntp.rb
@@ -7,12 +7,12 @@ ntp_package = attribute('ntp_package', default: 'openntpd', description: 'Check 
 ntp_servers = attribute(
   'ntp_servers',
   default: [
-    "pool.ntp.org"
+    'pool.ntp.org'
   ],
   description: 'list of ntp servers to use'
 )
 
-if "#{ntp_package}" == 'ntpd'
+if ntp_package.to_s == 'ntpd'
   ntp_service = 'ntpd'
   ntp_bin = '/usr/sbin/ntpd'
   ntp_drift_mode = '0640'
@@ -29,7 +29,7 @@ if "#{ntp_package}" == 'ntpd'
     ntp_user = 'ntp'
     ntp_conf = '/var/lib/ntp/drift'
   end
-elsif "#{ntp_package}" == 'openntpd'
+elsif ntp_package.to_s == 'openntpd'
   ntp_conf = '/etc/openntpd/ntpd.conf'
   ntp_user = 'ntpd'
   ntp_service = 'openntpd'
@@ -44,14 +44,14 @@ control 'ntp-1.0' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'ntpd should be present'
   desc 'Ensure ntpd executable and configuration are present'
-  if "#{ntp_package}" == 'ntpd' and os.darwin?
-    describe file("#{ntp_conf}") do
+  if ntp_package.to_s == 'ntpd' && os.darwin?
+    describe file(ntp_conf.to_s) do
       it { should be_file }
       its('content') { should match(/restrict default (kod nomodify notrap nopeer noquery|ignore)/) }
       its('content') { should match 'includefile /private/etc/ntp.conf' }
     end
-  elsif "#{ntp_package}" == 'ntpd'
-    describe file("#{ntp_conf}") do
+  elsif ntp_package.to_s == 'ntpd'
+    describe file(ntp_conf.to_s) do
       it { should be_file }
       its('content') { should match(/^disable monitor/) }
       its('content') { should match(/^(restrict default ignore|restrict -4 default ignore|restrict -4 default kod notrap nomodify nopeer noquery limited)/) }
@@ -63,41 +63,25 @@ control 'ntp-1.0' do                        # A unique ID for this control
         its('content') { should match(/^restrict #{server} default.*nomodify (notrap nopeer|nopeer notrap) noquery/) }
       end
     end
-    describe command("ntpstat") do
-      its('stdout') { should match 'synchronised to' }
-      its('stderr') { should eq '' }
-      its('exit_status') { should eq 0 }
-    end
-    describe command("timedatectl status") do
-      its('stdout') { should match 'NTP enabled: yes' }
-      its('stdout') { should match 'NTP synchronized: yes' }
-      its('stderr') { should eq '' }
-      its('exit_status') { should eq 0 }
-    end
-  elsif "#{ntp_package}" == 'openntpd'
-    describe file("#{ntp_conf}") do
+  elsif ntp_package.to_s == 'openntpd'
+    describe file(ntp_conf.to_s) do
       it { should be_file }
       its('content') { should match(/^listen on 127.0.0.1/) }
       ntp_servers.each do |server|
         its('content') { should match(/^servers #{server}/) }
       end
     end
-    describe command("#{ntp_bin} -n") do
-      its('stdout') { should eq '' }
-      its('stderr') { should eq 'configuration OK' }
-      its('exit_status') { should eq 0 }
-    end
   else
-    describe package("#{ntp_package}") do
+    describe package(ntp_package.to_s) do
       it { should be_installed }
     end
-    describe service("#{ntp_service}") do
+    describe service(ntp_service.to_s) do
       it { should_not be_enabled }
       it { should_not be_installed }
       it { should_not be_running }
     end
   end
-  describe file("#{ntp_bin}") do
+  describe file(ntp_bin.to_s) do
     it { should be_file }
     it { should be_executable }
     it { should be_owned_by 'root' }
@@ -106,11 +90,38 @@ end
 
 control 'ntp-2.0' do
   impact 0.7
+  title 'ntpd configuration should be valid'
+  desc 'Ensure ntpd configuration is correct'
+  if ntp_package.to_s == 'ntpd'
+    describe command('ntpstat') do
+      its('stdout') { should match 'synchronised to' }
+      its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
+    describe command('timedatectl status') do
+      its('stdout') { should match 'NTP enabled: yes' }
+      its('stdout') { should match 'NTP synchronized: yes' }
+      its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
+  elsif ntp_package.to_s == 'openntpd'
+    describe command("#{ntp_bin} -n") do
+      its('stdout') { should eq '' }
+      its('stderr') { should eq 'configuration OK' }
+      its('exit_status') { should eq 0 }
+    end
+  end
+end
+
+end
+
+control 'ntp-2.0' do
+  impact 0.7
   title 'ntpd should be running'
   desc 'Ensure ntpd is running'
   only_if { !(virtualization.role == 'guest' && virtualization.system == 'docker') }
-  describe processes("#{ntp_service}") do
-    its('users') { should eq ["#{ntp_user}"] }
+  describe processes(ntp_service.to_s) do
+    its('users') { should eq [ntp_user.to_s] }
     its('list.length') { should eq 1 }
   end
 end
@@ -120,10 +131,10 @@ control 'ntp-3.0' do
   title 'ntpd should have drift file'
   desc 'Ensure ntpd drift file is present'
   only_if { !(virtualization.role == 'guest' && virtualization.system == 'docker') }
-  describe file("#{ntp_drift}") do
+  describe file(ntp_drift.to_s) do
     it { should be_file }
-    it { should be_owned_by "#{ntp_user}" }
-    its('mode') { should cmp "#{ntp_drift_mode}" }
+    it { should be_owned_by ntp_user.to_s }
+    its('mode') { should cmp ntp_drift_mode.to_s }
   end
 end
 
@@ -132,7 +143,7 @@ control 'ntp-4.0' do
   title 'ntpd updated drift files'
   desc 'Ensure ntpd drift file is updated and less than 8h in the past'
   only_if { !(virtualization.role == 'guest' && virtualization.system == 'docker') }
-  describe file("#{ntp_drift}").mtime.to_i do
+  describe file(ntp_drift.to_s).mtime.to_i do
     it { should <= Time.now.to_i }
     it { should >= Time.now.to_i - 28800 }
   end
